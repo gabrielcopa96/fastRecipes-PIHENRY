815b40964f6b61fd2c36c09426de5b3c6cf5551f
Gabriel <gabrielcncopa@gmail.com>
2022-06-20 03:51:24 -0300

    enia

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 
 import store from './redux/store';
 
